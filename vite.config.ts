0378180c6a5af68db5ea6e0555cb6b99d2d8f97c
Nihar Das Roy <swapandasroy972@gmail.com>
2026-01-22 04:28:04 +0600

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/website/',   // 🔥 এটা অবশ্যই লাগবে
+  base: '/website/',
+  // 🔥 এটা অবশ্যই লাগবে
 })
 
 
