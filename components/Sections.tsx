b7407bf161897957c7321a9823a8dd5fd9158a29
Nihar Das Roy <swapandasroy972@gmail.com>
2026-01-22 13:53:25 +0600

    final github pages setup

--- components/Sections.tsx
+++ components/Sections.tsx
@@ -49,10 +49,13 @@ export const Hero: React.FC = () => {
       </div>
       <div className="w-1/2 relative flex justify-end">
         <div className="absolute right-0 top-1/2 -translate-y-1/2 w-[600px] h-[600px] bg-[#FFD700] rounded-full blur-[150px] opacity-10"></div>
-  <img src={`${import.meta.env.BASE_URL}${p}1.jpg`}
-alt="About Nihar"
+ <img
+  src={`${import.meta.env.BASE_URL}1.jpg`}
+  alt="About Nihar"
   className="w-full h-[650px] object-cover rounded-xl"
 />
+
+
 
       </div>
     </section>
@@ -64,11 +67,12 @@ export const About: React.FC = () => {
     <section id="about" className="py-32 px-20 bg-[#0a0a0a]">
       <div className="flex gap-20 items-center">
         <div className="w-2/5 relative">
-  <img src={`${import.meta.env.BASE_URL}${p}2.jpg`}
-
+ <img
+  src={`${import.meta.env.BASE_URL}2.jpg`}
   alt="About Nihar"
   className="w-full h-[650px] object-cover rounded-xl"
 />
+
 
 
           <div className="absolute -bottom-10 -right-10 bg-[#FFD700] p-10 text-black rounded-xl shadow-2xl">
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/website1/",
   plugins: [react()],
-  base: '/website/',
-  // 🔥 এটা অবশ্যই লাগবে
-})
+});
 
 
 
